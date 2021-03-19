43c40f63d8e0bc7af56c8d23f083131d8ff0b0b0
AzureAD\SauloBezerraMoura <moura.bsaulo@gmail.com>
2021-03-19 15:57:54 -0300

    Adicionando textos

--- bin/dartgit.dart
+++ bin/dartgit.dart
@@ -2,4 +2,5 @@ import 'package:dartgit/dartgit.dart' as dartgit;
 
 void main(List<String> arguments) {
   print('Hello world, tudo bem? ${dartgit.calculate()}!');
+  print('Bem vindo ao mundo!');
 }
